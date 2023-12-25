8bab4e81bc01b6560be5dd7266d3a752949f0846
karinavermaa <20kv15@queensu.ca>
2023-12-25 01:03:42 -0500

    Getting environment set up.

---
+++ src/Components/NavBar.js
@@ -0,0 +1,17 @@
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
+
+function NavBar() {
+
+    return (
+        <>
+        <nav className="NavBar">
+            <div className="NavBar-container">
+                
+            </div>
+        </nav>
+        </>
+    )
+
+}
+export default NavBar
